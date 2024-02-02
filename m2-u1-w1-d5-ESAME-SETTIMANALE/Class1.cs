8258c29d0a9397eb9fc4e2de63fade7378498073
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-02-02 16:45:23 +0100

    Aggiungere i file di progetto.

---
+++ m2-u1-w1-d5-ESAME-SETTIMANALE/Class1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace m2_u1_w1_d5_ESAME_SETTIMANALE
+{
+    public class Contribuente
+    {
+        // Proprietà della classe
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public double RedditoAnnuale { get; set; }
+
+        // Costruttori della classe
+        public Contribuente() { }
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale,
+                                          char sesso, string comuneResidenza, double redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        // Metodi comuni per settare le proprietà della classe Contribuente
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+        public void SetCognome(string cognome)
+        {
+            Cognome = cognome;
+        }
+        public void SetDataNascita(DateTime dataNascita)
+        {
+            DataNascita = dataNascita;
+        }
+        public void SetCodiceFiscale(string codiceFiscale)
+        {
+            CodiceFiscale = codiceFiscale;
+        }
+        public void SetSesso(char sesso)
+        {
+            Sesso = sesso;
+        }
+        public void SetComuneResidenza(string comuneResidenza)
+        {
+            ComuneResidenza = comuneResidenza;
+        }
+        public void SetRedditoAnnuale(double redditoAnnuale)
+        {
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        // Metodo per calcolare l'imposta in base al reddito annuale
+        public double CalcolaImposta()
+        {
+            if (RedditoAnnuale <= 15000)
+            {
+                return RedditoAnnuale * 0.23;
+            }
+            else if (RedditoAnnuale <= 28000)
+            {
+                return 3450 + ((RedditoAnnuale - 15000) * 0.27);
+            }
+            else if (RedditoAnnuale <= 55000)
+            {
+                return 6960 + ((RedditoAnnuale - 28000) * 0.38);
+            }
+            else if (RedditoAnnuale <= 75000)
+            {
+                return 17220 + ((RedditoAnnuale - 55000) * 0.41);
+            }
+            else
+            {
+                return 25420 + ((RedditoAnnuale - 75000) * 0.43);
+            }
+        }
+
+        //metodi per la formattazione dei dati
+        //metodo per rendere la prima lettera maiuscola e il resto minuscola
+        public static string PrimaLetteraMaiuscola(string testo)
+        {
+            if (!string.IsNullOrEmpty(testo))
+            {
+                return char.ToUpper(testo[0]) + testo.Substring(1).ToLower();
+            }
+            return testo;
+        }
+
+        // metodi per raccogliere dati utente tramite imput e controlli
+        //mtodo per controllare che l'input sia una stringa non vuota e con determinati caratteri
+        private string RichiediStringaNonVuota(string messaggio)
+        {
+            string input;
+            do
+            {
+                Console.Write(messaggio);
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Il campo non può essere vuoto, riprova.");
+                    Console.ResetColor();
+                }
+                else if (!Regex.IsMatch(input, @"^[\p{L}']+$")) // tutte le lettere minuscole maiuscole con e senza accenti e l 'apostrofo
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Il campo deve contenere solo lettere (anche accentate) o apostrofi, riprova.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    return input;
+                }
+            } while (true);
+        }
+
+        //metodo per controllare che l'input sia una data nel formato gg/mm/aaaa
+        private DateTime RichiediDataNascita()
+        {
+            DateTime dataNascita;
+            DateTime dataMinima = new DateTime(1850, 1, 1);
+            do
+            {
+                Console.Write("Inserisci la tua data di nascita (formato gg/mm/aaaa): ");
+                if (DateTime.TryParse(Console.ReadLine(), out dataNascita) && dataNascita < DateTime.Now && dataNascita > dataMinima)
+                {
+                    return dataNascita;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Formato data non valido, riprova.");
+                    Console.ResetColor();
+                }
+            } while (true);
+        }
+
+        //metodo per controllare che l'input sia un carattere M o F (anche minuscolo)
+        private char RichiediSesso()
+        {
+            char sessoInput;
+            do
+            {
+                Console.Write("Inserisci il tuo sesso (M/F): ");
+                string input = Console.ReadLine();
+                sessoInput = input.ToUpper()[0];
+                if (sessoInput == 'M' || sessoInput == 'F')
+                {
+                    return sessoInput;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Formato sesso non valido, inserire 'M' o 'F'.");
+                    Console.ResetColor();
+                }
+            } while (true);
+        }
+
+        //metodo per controllare che l'input sia un numero decimale (double) e positivo 
+        private double RichiediRedditoAnnuale()
+        {
+            double redditoAnnuale;
+            do
+            {
+                Console.Write("Inserisci il tuo reddito annuale: ");
+                if (double.TryParse(Console.ReadLine(), out redditoAnnuale) && redditoAnnuale > 0)
+                {
+                    return redditoAnnuale;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Formato del reddito non corretto, riprova.");
+                    Console.ResetColor();
+                }
+            } while (true);
+        }
+
+        //metodo per controllare che l'input sia una stringa di 16 caratteri e non vuota
+        private string RichiediCodiceFiscale()
+        {
+            string codiceFiscale;
+            do
+            {
+                Console.Write("Inserisci il tuo codice fiscale: ");
+                codiceFiscale = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(codiceFiscale) && codiceFiscale.Length == 16)
+                {
+                    return codiceFiscale;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Il codice fiscale deve essere di 16 caratteri, riprova.");
+                    Console.ResetColor();
+                }
+            } while (true);
+        }
+
+        //metodo per raccogliere tutti i dati utente tramite i metodi sopra e settare le proprietà della classe Contribuente
+        public void DataSet()
+        {
+            SetNome(RichiediStringaNonVuota("Inserisci il tuo nome: "));
+            SetCognome(RichiediStringaNonVuota("Inserisci il tuo cognome: "));
+            SetDataNascita(RichiediDataNascita());
+            SetCodiceFiscale(RichiediCodiceFiscale());
+            SetSesso(RichiediSesso());
+            SetComuneResidenza(RichiediStringaNonVuota("Inserisci il tuo comune di residenza: "));
+            SetRedditoAnnuale(RichiediRedditoAnnuale());
+            Console.Clear();
+        }
+    }
+}
+
---
+++ m2-u1-w1-d5-ESAME-SETTIMANALE/Class2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace m2_u1_w1_d5_ESAME_SETTIMANALE
+{
+    public static class Menu
+    {
+        public static void Start()
+        {
+            Contribuente contribuente = new Contribuente();
+            string resp;
+            //aggiunta per visualizzare correttamente i caratteri speciali come quelli dell'euro
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Si desidera continuare con il calcolo dell'imposta? (y/n)");
+                resp = Console.ReadLine().ToLower();
+                if (resp == "y")
+                {
+                    Console.Clear();
+                    contribuente.DataSet();
+                    Console.WriteLine("==================================================");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
+                    Console.WriteLine("\n");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Contribuente: {Contribuente.PrimaLetteraMaiuscola(contribuente.Nome)} " +
+                                      $"{Contribuente.PrimaLetteraMaiuscola(contribuente.Cognome)},");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine($"nato il: {contribuente.DataNascita.ToShortDateString()}({contribuente.Sesso}),");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine($"residente in: {Contribuente.PrimaLetteraMaiuscola(contribuente.ComuneResidenza)},");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine($"codice fiscale: {contribuente.CodiceFiscale.ToUpper()},");
+                    Console.WriteLine("\n");
+
+                    Console.Write($"Reddito dichiarato: {contribuente.RedditoAnnuale},");
+                    Console.WriteLine("\n");
+
+                    Console.WriteLine($"IMPOSTA DA VERSARE: € {contribuente.CalcolaImposta():N2}");
+                    Console.WriteLine("\n");
+
+                    Console.ResetColor();
+                }
+                else if (resp == "n")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Grazie e Arrivederci!");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Scelta non valida! Premi un tasto per continuare.");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine("Premi un tasto per tornare al menú inizale.");
+                Console.ReadKey();
+            } while (resp != "n");
+        }
+    }
+}
