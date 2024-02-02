b2c2b255dfcb118b21bd0b1275bdaa9dd7905222
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-02-02 17:32:24 +0100

    deleted the superfluous

--- m2-u1-w1-d5-ESAME-SETTIMANALE/Program.cs
+++ m2-u1-w1-d5-ESAME-SETTIMANALE/Program.cs
@@ -5,69 +5,6 @@
         static void Main(string[] args)
         {
             Menu.Start();
-
-
-
-
-
-
-            /* Contribuente contribuente = new Contribuente();
-             string resp = "";
-
-             do
-             {
-                 Console.Clear();
-                 Console.WriteLine("Si desidera continuare con il calcolo dell'imposta? (y/n)");
-                 resp = Console.ReadLine().ToLower();
-                 if (resp == "y")
-                 {
-                     Console.Clear();
-                     contribuente.DataSet();
-                     Console.WriteLine("==================================================");
-                     Console.WriteLine("\n");
-                     Console.WriteLine("CALCOLO DELL'IMPOSTA DA VERSARE:");
-                     Console.WriteLine("\n");
-                     Console.WriteLine($"Contribuente: {contribuente.Nome} {contribuente.Cognome}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine($"Nato il: {contribuente.DataNascita.ToShortDateString()}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine($"Residente in: {contribuente.ComuneResidenza}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine($"Codice Fiscale: {contribuente.CodiceFiscale}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine($"Reddito dichiarato: € {contribuente.RedditoAnnuale:N2}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine($"IMPOSTA DA VERSARE: € {contribuente.CalcolaImposta():N2}");
-                     Console.WriteLine("\n");
-
-                     Console.WriteLine("Premi un tasto per tornare al menú inizale.");
-                     Console.ReadKey();
-                 }
-                 else if (resp == "n")
-                 {
-                     Console.WriteLine("Grazie e Arrivederci!");
-                     break;
-                 }
-                 else
-                 {
-                     Console.WriteLine("Scelta non valida! Premi un tasto per continuare.");
-                     Console.ReadKey();
-                 }
-
-                 Console.WriteLine("Premi un tasto per chiudere l'app");
-                 Console.ReadKey();
-             } while (resp != "n");
-
-
-         }
-     }
- }
- */
         }
     }
 }
